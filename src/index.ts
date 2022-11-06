83265a65f6406910b78b74ea77bd870d059ed533
lioarce01 <lioarce1@gmail.com>
2022-11-06 19:55:26 -0300

    login added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { auth } from "express-openid-connect";
+import { requiresAuth } from "express-openid-connect";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -26,6 +28,12 @@ app.get("/posts", async (req, res, next) => {
   }
 });
 
+// get a profile
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+})
+
 // get all users
 app.get("/users", async (req, res, next) => {
   try {
@@ -47,13 +55,22 @@ app.post("/posts", async (req, res, next) => {
     const post = await prisma.post.create({
       // crear un post desde el id del usuario que lo crea
       data: {
-        authorId: req.body.authorId,
+        author: {
+          connect: {
+            id: req.body.authorId,
+          },
+        },
         title: req.body.title,
         published: req.body.published,
       }
     });
 
-    res.json({ post });
+    if (!post) {
+      return res.json({ message: "This title already exists" });
+    } else {
+      res.json({ post });
+    }
+
   } catch (error: any) {
     next(error.message);
   }
@@ -85,6 +102,7 @@ app.patch("/posts/:id", async (req, res, next) => {
     });
 
     res.json({ post });
+
   } catch (error: any) {
     next(error.message);
   }
@@ -128,6 +146,26 @@ app.get("/users/:id/posts", async (req, res, next) => {
     next(error.message);
   }
 });
+
+//////////////////////////////////////////////
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: '8CZpCFmQwIc6FRCrKPJNl5pIZK2n2k5F',
+  issuerBaseURL: 'https://dev-gdm-hbgx.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 
 app.listen(3000, () => {
   console.log("App listening on port 3000");
